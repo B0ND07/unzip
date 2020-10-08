0f55adcf637822f7c59dc5a876248a174f89d988
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2020-10-08 15:26:31 +0900

    Test1

--- plugins/unzip.py
+++ plugins/unzip.py
@@ -8,12 +8,12 @@ import os
 import shutil
 import subprocess
 import time
-from helper_funcs.display_progress import humanbytes, progress_for_pyrogram
+
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from sample_config import Config
+    from config import Config
 
 # the Strings used for this "thing"
 from translation import Translation
@@ -27,14 +27,14 @@ from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["unzip"]))
 async def unzip(bot, update):
-    TRChatBase(update.from_user.id, update.text, "unzip")
-    if str(update.from_user.id) in Config.AUTH_USERS:
-        await bot.send_message(
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
             chat_id=update.chat.id,
-            text=Translation.NOT_AUTH_USER_TEXT,
-            reply_to_message_id=update.message_id
+            message_ids=update.message_id,
+            revoke=True
         )
         return
+    TRChatBase(update.from_user.id, update.text, "unzip")
     saved_file_path = Config.DOWNLOAD_LOCATION + \
         "/" + str(update.from_user.id) + ".unzip.zip"
     if os.path.exists(saved_file_path):
@@ -54,7 +54,11 @@ async def unzip(bot, update):
                 message=reply_message,
                 file_name=saved_file_path,
                 progress=progress_for_pyrogram,
-                progress_args=(Translation.DOWNLOAD_START, a.message_id, update.chat.id, c_time)
+                progress_args=(
+                    Translation.DOWNLOAD_START,
+                    a,
+                    c_time
+                )
             )
         except (ValueError) as e:
             await bot.edit_message_text(
@@ -91,18 +95,18 @@ async def unzip(bot, update):
                 # https://stackoverflow.com/a/26178369/4723940
             except:
                 try:
-                    #os.remove(saved_file_path)
+                    os.remove(saved_file_path)
                     shutil.rmtree(extract_dir_path)
                 except:
                     pass
-              #  await bot.edit_message_text(
-                 #   chat_id=update.chat.id,
-                 #   text=Translation.EXTRACT_ZIP_ERRS_OCCURED,
-                #    disable_web_page_preview=True,
-             #       parse_mode="html",
-           #         message_id=a.message_id
-       #         )
-      #      else:
+                await bot.edit_message_text(
+                    chat_id=update.chat.id,
+                    text=Translation.EXTRACT_ZIP_ERRS_OCCURED,
+                    disable_web_page_preview=True,
+                    parse_mode="html",
+                    message_id=a.message_id
+                )
+            else:
                 os.remove(saved_file_path)
                 inline_keyboard = []
                 zip_file_contents = os.listdir(extract_dir_path)
